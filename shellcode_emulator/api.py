8df4189d75a8c50cd28303f7b5603a0059e9eaf1
Matt Oh <oh.jeongwook@gmail.com>
2020-05-27 17:12:29 -0700

    Added log_address_range

--- shellcode_emulator/api.py
+++ shellcode_emulator/api.py
@@ -8,6 +8,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import logging
 import traceback
 import pprint
+import json
 
 from unicorn import *
 from unicorn.x86_const import *
@@ -20,13 +21,29 @@ logger = logging.getLogger(__name__)
 
 class Hook:
     amd64_argument_regs = (UC_X86_REG_RCX, UC_X86_REG_RDX, UC_X86_REG_R8, UC_X86_REG_R9)
-    def __init__(self, emulator, arch):
+    def __init__(self, emulator, arch, debug = 1):
+        self.debug = debug
         self.emulator = emulator
         self.arch = arch
         self.uc = emulator.uc
         self.trace_target_modules = ['ntdll', 'kernel32', 'kernelbase']
         self.last_code_information = {}
         self.windows_api_resolver = windbgtool.windows_api.Resolver()
+        self.api_log = []
+        self.log_addresses = []
+
+    def add_log_address_range(self, start, end):
+        self.log_addresses.append((start, end))
+
+    def check_log_address(self, address):
+        if len(self.log_addresses) == 0:
+            return True
+
+        for (start, end) in self.log_addresses:
+            if start <= address and address <= end:
+                return True
+
+        return False
 
     def return_function(self, uc, return_address, arg_count, return_value):
         print('Return Address: %x' % (return_address))
@@ -53,36 +70,55 @@ class Hook:
         return argument_values
 
     def log_arguments(self, function_def, prefix = '    '):
+        arguments = []
         if function_def['arguments'] == None:
-            return
+            return arguments
 
         index = 0
         argument_values = self.get_arguments(len(function_def['arguments']))
 
-        for argument in function_def['arguments']:
-            if 'name' in argument:
-                print(prefix + 'name: ' + argument['name'])
+        for argument_def in function_def['arguments']:
+            argument = {}
+            if 'name' in argument_def:
+                if self.debug > 0:
+                    print(prefix + 'name: ' + argument_def['name'])
 
-            argument_value = argument_values[index]
-            print(prefix + '    ' + hex(argument_value))
+                argument['name'] = argument_def['name']
 
-            if argument['type'] in ('LPCWSTR', 'LPWSTR'):
+            argument['type'] = argument_def['type']
+            argument['value'] = argument_value = argument_values[index]
+
+            if self.debug > 0:
+                print(prefix + '    ' + hex(argument_value))
+
+            if argument_def['type'] in ('LPCWSTR', 'LPWSTR'):
                 if argument_value != 0:
                     try:
-                        print(prefix + '    '+self.emulator.memory.read_wstring(argument_value))
+                        argument['detailed_value'] = self.emulator.memory.read_wstring(argument_value)
+                        if self.debug > 0:
+                            print(prefix + '    ' + argument['detailed_value'])
                     except:
                         print(prefix + '    Exception to read memory: %x' % argument_value)
 
-            elif argument['type'] in ('LPCSTR', 'LPSTR'):
+            elif argument_def['type'] in ('LPCSTR', 'LPSTR'):
                 if argument_value != 0:
                     try:
-                        print(prefix + '    '+self.emulator.memory.read_string(argument_value))
+                        argument['detailed_value'] = self.emulator.memory.read_string(argument_value)
+                        if self.debug > 0:
+                            print(prefix + '    ' + argument['detailed_value'])
                     except:
                         print(prefix + '    Exception to read memory: %x' % argument_value)
-
+                        
+            arguments.append(argument)
             index += 1
 
+        return arguments
+
     def callback(self, uc, address, size, user_data):
+        return_address, = self.emulator.memory.get_stack(1, False)
+        if not self.check_log_address(return_address):
+            return
+
         code = uc.mem_read(address, size)
         try:
             name = self.emulator.debugger.find_symbol(address)
@@ -90,8 +126,8 @@ class Hook:
             name = ''
             self.emulator.instruction.dump_disassembly(address, size, find_symbol = True)
 
-        sp = self.uc.reg_read(self.emulator.register.get_by_name("sp"))
-        print('%x: %s (%x)' % (sp, name, address))
+        sp = uc.reg_read(self.emulator.register.get_by_name("sp"))
+        print('%x: %s (%x) - return: %x' % (sp, name, address, return_address))
 
         name_tokens = name.split('!')
 
@@ -103,7 +139,16 @@ class Hook:
         function_def = self.windows_api_resolver.find_function(function_name)
 
         if function_def:
-            self.log_arguments(function_def)
+            arguments = self.log_arguments(function_def)
+        else:
+            arguments = []
+
+        self.api_log.append({'name': name, 'arguments': arguments})
+
+        if name == 'kernel32!WinExec':
+            ip = self.emulator.register.get_by_name("ip")
+            print("Writing %x -> %x" % (return_address, ip))
+            uc.reg_write(ip, return_address)
 
         if name == 'ntdll!LdrLoadDll':
             try:
@@ -215,7 +260,7 @@ class Hook:
             fd.close()
             """
 
-            #self.uc.add_unicorn_hook(UC_HOOK_CODE, self.dump_memory_callback, None, return_address, return_address+1)
+            #self.uc.add_hook(UC_HOOK_CODE, self.dump_memory_callback, None, return_address, return_address+1)
 
         elif name == 'kernel32!GetFileSize':
             (return_address, hfile, lp_file_size_high) = self.emulator.memory.get_stack(uc, 2)
@@ -230,8 +275,6 @@ class Hook:
             #uc.hook_add(UC_HOOK_CODE, self.dump_memory_callback, None, return_address, return_address+1)
             self.return_function(uc, return_address, 2, 0x7bafe)
 
-        #kernel32!CreateFileMappingA
-        #kernel32!MapViewOfFile
         if code == '\x0f\x34': #sysenter
             asm = self.uc.Instruction.disassemble(code, address)
 
@@ -251,8 +294,16 @@ class Hook:
     def start(self):
         self.emulator.debugger.load_symbols(self.trace_target_modules)
 
+        hooked_addresses = {}
         for trace_module in self.trace_target_modules:
             for (symbol, address) in self.emulator.debugger.symbol_to_address.items():
-                logger.debug("api.Hook.start: %s - %s (%x)", trace_module, symbol, address)
-                self.emulator.add_unicorn_hook(UC_HOOK_CODE, self.callback, trace_module, address, address)
+                if address in hooked_addresses:
+                    continue
 
+                hooked_addresses[address] = 1
+                logger.debug("api.Hook.start: %s - %s (%x)", trace_module, symbol, address)
+                self.emulator.add_hook(UC_HOOK_CODE, self.callback, trace_module, address, address)
+
+    def save(self, filename):
+        with open(filename, 'w') as fd:
+            json.dump(self.api_log, fd, indent = 4)
